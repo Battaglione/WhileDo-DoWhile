c5e2e4a0410ed1ac01dc88315268c40635ffb26f
Azel04 <75199595+Azel04@users.noreply.github.com>
2020-12-01 09:02:53 +0100

    Add files via upload

---
+++ dowhile1.asm
@@ -0,0 +1,32 @@
+;Rossetti Jacopo
+;3INA
+;1-12-2020
+
+TITLE ciclo while do 
+.MODEL SMALL    
+    
+.DATA 
+    conta     DB    ?
+    vettore   DB    01h,'A' ,02h,03h,'A' ,0h,05h,'A' ,0h,07h,08h,09h,0Bh,'A',45,71,54,6,65,65,11,1,2,3,4,5,1,'$'  
+.CODE                                                                                                
+    
+.STARTUP         
+    lea si,vettore 
+    mov conta,00h   
+ciclo:        
+    mov al,[si]
+    cmp al,'$'
+    ja nonuguale
+    jb nonuguale
+    jmp saltafine 
+nonuguale:
+    inc conta
+    inc si
+    jmp ciclo
+saltafine: 
+    inc conta
+;servizio DOS di chiusura programma
+    mov ax, 4c00h ; exit to operating system.
+    int 21h    
+END
+
